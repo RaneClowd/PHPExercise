8fc4917df4b36aab43908642b99f18d8adec097e
Kenny <kskaggs@solstice-consulting.com>
2016-02-25 12:55:13 -0600

    getting some syntax under my belt

---
+++ chickenman.php
@@ -0,0 +1,27 @@
+<html>
+
+    <head>
+
+        <title>Question</title>
+
+    </head>
+
+    <body>
+
+        <form action="submit.php" method=post>
+
+            My name is:
+
+            <br> <input type="text" name="Name">
+
+            I think chicken man is:
+
+            <br /><input type="text" name="Answer">
+
+            <input type="submit" name="submit" value="That's right!">
+
+        </form>
+
+    </BODY>
+
+</html>
---
+++ submit.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+		<title>Sorry</title>
+	</head>
+
+	<?php
+		$nameEntered = $_REQUEST['Name'];
+		$answerEntered = $_REQUEST['Answer'];
+	?>
+
+	<body bgcolor="#000000" text="#ffffff">
+
+		<p>
+			You can think it's <?php print $answerEntered; ?> all you want, <?php print $nameEntered; ?>! But we all know who the real chickenman is!
+
+	</body>
+
+</html>
