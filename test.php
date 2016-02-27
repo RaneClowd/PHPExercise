5bbebcc3dbc5f70d5ef6a4f7126952f5b2894267
unknown <skaggskd88@gmail.com>
2016-02-26 22:32:27 -0600

    displaying a button for each homeroom

--- test.php
+++ test.php
@@ -1,21 +1,27 @@
 <html>
-<head>
-    <title>Sorry</title>
-</head>
+    <head>
+        <title>Sorry</title>
+    </head>
 
 
-<body bgcolor="#000000" text="#ffffff">
+    <body bgcolor="#000000" text="#ffffff">
 
-<?php
-    include 'DataStore.php';
 
-    $dataStore = new DataStore();
 
-    foreach ($dataStore->homeroomNames() as $homeroomName) {
-        echo $homeroomName . "<br>";
-    }
-?>
+        <form action="test.php" method=get>
 
-</body>
+            <?php
+            include 'DataStore.php';
+
+            $dataStore = new DataStore();
+
+            foreach ($dataStore->homeroomNames() as $homeroomName) {
+                echo "<input type=\"submit\" name=\"homeroom\" value=\"" . $homeroomName . "\">";
+            }
+            ?>
+
+        </form>
+
+    </body>
 
 </html>
